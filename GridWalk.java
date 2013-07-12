2f750a6ecdc2b4344b21b65574f4116a08a4c3a8
yao23 <myspiritcrazy@gmail.com>
2013-07-12 15:09:48 -0400

    find neighboor point when cannot move forward running recursively

--- GridWalk.java
+++ GridWalk.java
@@ -7,13 +7,15 @@ public class GridWalk {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int i, j, max_coordinate = 298, counter = 0;		
-		for (i = 1; i <= max_coordinate; i++) { // x axis, from 1 to (max_coordinate-1)
+/*		for (i = 1; i <= max_coordinate; i++) { // x axis, from 1 to (max_coordinate-1)
 			for (j = 0; j <= (max_coordinate-i); j++) {// y axis, from (max_coordinate-i) to 1 
-				if( beyond_nineteen(i, j) ) break;
-				//if( beyond_nineteen(i, j) ) j = max_coordinate;
+				//if( beyond_nineteen(i, j) ) break;
+				if( beyond_nineteen(i, j) ) j = max_coordinate;
 				else counter++;
 			}
 		}
+*/
+		counter = search_point(1, 0, max_coordinate, counter);
 		counter *= 4; // 4 areas divided by 2 axes
 		counter++; // the origin (0, 0)
 		
@@ -21,10 +23,20 @@ public class GridWalk {
 
 	}
 	
+	public static int search_point(int x, int y, int max_coordinate, int counter) {
+		int i, j;
+		for (i = x; i <= max_coordinate; i++) { // x axis, from 1 to (max_coordinate-1)
+			for (j = y; j <= (max_coordinate-i); j++) {// y axis, from (max_coordinate-i) to 1 
+				if( beyond_nineteen(i, j) ) counter = search_point(i+1, j-1, j-1, counter);				
+				else counter++;
+			}
+		}
+		return counter;
+	}
+	
 	public static boolean beyond_nineteen(int x, int y) {
-		int x_tmp = x, y_tmp = y;
-
-		int digits_sum = 0;
+		int x_tmp = x, y_tmp = y, digits_sum = 0;
+		
 		do {
 			digits_sum += (x_tmp % 10);
 			x_tmp /= 10;
